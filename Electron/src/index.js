5aeb48a301c76bd1f12fbb96a0b35b6ea23b6641
Julio Preciado <julioplx2@gmail.com>
2019-10-03 23:25:25 -0500

    Login

--- Electron/src/index.js
+++ Electron/src/index.js
@@ -1,4 +1,8 @@
-const mysql = require('mysql')
+const mysql = require('mysql');
+const { app, BrowserWindow } = require('electron');
+const url = require('url');
+const path = require('path');
+const submitFormButton = document.querySelector("#login");
 
 var connection = mysql.createConnection({
     host: 'remotemysql.com',
@@ -14,3 +18,42 @@ connection.connect(function (err) {
         console.log(err.fatal);
     }
 });
+
+submitFormButton.addEventListener("submit", function(event){
+    event.preventDefault();   // stop the form from submitting
+    return Response('hola');
+});
+
+/* Perform a query
+$query = 'SELECT * FROM `myTableName` LIMIT 10';
+
+connection.query($query, function(err, rows, fields) {
+    if(err){
+        console.log("An error ocurred performing the query.");
+        console.log(err);
+        return;
+    }
+
+    console.log("Query succesfully executed", rows);
+});
+
+// Close the connection
+connection.end(function(){
+    // The connection has been closed
+});
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: false
+        }
+    });
+    mainWindow.setMenuBarVisibility(false);
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'views/home.html'),
+        protocol: 'file',
+        slashes: true,
+    }))
+});*/
---
+++ php/pass_hash.php
@@ -0,0 +1,6 @@
+<?php
+//Create pass
+$passHash = password_hash('luisesgay1', PASSWORD_BCRYPT);
+
+//Verify pass
+password_verify('luisesgay1', '$2y$10$ISMMFLcvt4pZEYH4Cl08B.v/8r7N2k1ZZIPyEx1SyvyPlrwtFd5mW')
