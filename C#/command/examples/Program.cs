2a4ef8d7c4b733797f8d4b947d7499b24017c414
Julio Preciado <julioplx2@gmail.com>
2019-10-31 22:44:04 -0600

    changues

--- C#/command/examples/Program.cs
+++ C#/command/examples/Program.cs
@@ -6,7 +6,59 @@ namespace examples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            float[][] jaggedArray= new float[8][];
+            float[] arrayAux = new float[50];
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                ;
+                int cont = 0;
+                bool bandera = false;
+                do
+                {
+                    Console.WriteLine("---Inserte los valores del jaggedArray fila numero {0}---", i);
+                    string valor = Console.ReadLine();
+                    if (float.TryParse(valor, out float valorF))
+                    {
+                        arrayAux[cont] = valorF;
+                        Console.WriteLine("¡¡¡Valor insertado en la fila {0} columna {1} !!!", i, cont);
+                        int opc;
+                        do
+                        {
+                            Console.WriteLine("¡¡¡Desea insertar un valor más? SI(1) / NO(0) !!!");
+                            _ = Console.ReadLine();
+                            if (int.TryParse(valor, out opc))
+                            {
+                                if (opc == 1)
+                                {
+                                    Console.WriteLine("---------");
+                                }
+                                else if (opc == 0)
+                                {
+                                    bandera = true;
+                                    arrayAux = new float[50];
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Input invalido");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Input invalido");
+                            }
+                        } while (opc != 0);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Input invalido");
+                    }
+
+                } while (bandera != true);
+            }
+
+            Console.WriteLine(jaggedArray);
+
         }
     }
 }
