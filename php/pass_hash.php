54d6042d9c2128c12168323dccd2e309752d4df3
Julio Preciado <julioplx2@gmail.com>
2019-11-23 00:26:58 -0600

    Leer Excel en php

--- php/pass_hash.php
+++ php/pass_hash.php
@@ -4,3 +4,7 @@ $passHash = password_hash('luisesgay1', PASSWORD_BCRYPT);
 
 //Verify pass
 password_verify('luisesgay1', '$2y$10$ISMMFLcvt4pZEYH4Cl08B.v/8r7N2k1ZZIPyEx1SyvyPlrwtFd5mW');
+
+?>
+
+<h1>Hola</h1>
