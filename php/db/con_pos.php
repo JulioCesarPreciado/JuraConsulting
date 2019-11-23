1d2c5ab09e94d8d654c2aa9608a35c0e6996a821
Julio Preciado <julioplx2@gmail.com>
2019-11-23 01:10:27 -0600

    cargar excel

--- php/db/con_pos.php
+++ php/db/con_pos.php
@@ -1,8 +1,8 @@
 <?php
 
 //Leer excel
-require_once 'lib/PHPExcel/Classes/PHPExcel.php';
-$archivo = "libro1.xlsx";
+require_once '../lib/PHPExcel-1.8/Classes/PHPExcel.php';
+$archivo = "../doc/test.xlsx";
 $inputFileType = PHPExcel_IOFactory :: identify($archivo);
 $objReader = PHPExcel_IOFactory :: createReader($inputFileType);
 $objPHPExcel = $objReader -> load($archivo);
@@ -14,5 +14,19 @@ $highestColumn = $sheet -> getHighestColumn();
 $conn = pg_connect(
   "host=localhost port=5432 dbname=Pruebas user=julio password=julio"
 );
+
+if($conn){
+    for ($row = 2; $row <= $highestRow; $row++){ 
+        $id = $sheet->getCell("A".$row)->getValue();
+        $nombre = $sheet->getCell("B".$row)->getValue();
+        $precio_total = $sheet->getCell("C".$row)->getValue();
+        $qry = "INSERT INTO main  (id,nombre, precio_total, create_date,write_date) VALUES ($id,'$nombre', $precio_total,NOW(),NOW())";
+        //echo $qry; die();
+        $result = pg_query($conn, $qry);
+    }
+    echo 'Exito';    
+}else{
+    echo 'Error';
+}
     
 ?>
