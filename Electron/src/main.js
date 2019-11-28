b25a2971d8dde20284ac126614a3c88013f30554
Julio Preciado <julioplx2@gmail.com>
2019-11-27 23:28:18 -0600

    cambios radicales electron

--- Electron/src/main.js
+++ Electron/src/main.js
@@ -12,6 +12,7 @@ app.on('ready', () => {
         icon: path.join(__dirname, 'assets/icons/png/favicon-96x96.png'),
         //frame: false
     });
+    mainWindow.setMenuBarVisibility(false)
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'views/index.html'),
         protocol: 'file',
