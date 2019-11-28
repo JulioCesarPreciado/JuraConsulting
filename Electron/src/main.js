cf38ef1dc26c919b3277662c3c1fbe05769160a5
Julio Preciado <julioplx2@gmail.com>
2019-11-27 23:08:42 -0600

    fads

--- Electron/src/main.js
+++ Electron/src/main.js
@@ -7,14 +7,14 @@ let mainWindow;
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
         },
-        icon: path.join(__dirname, 'assets/icons/png/favicon-96x96.png')
+        icon: path.join(__dirname, 'assets/icons/png/favicon-96x96.png'),
+        //frame: false
     });
-    mainWindow.setMenuBarVisibility(false);
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'views/index.html'),
         protocol: 'file',
-        slashes: true,
+        slashes: true
     }))
 });
